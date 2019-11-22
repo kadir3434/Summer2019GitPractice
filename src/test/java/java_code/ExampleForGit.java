b2415661922aae16b6e5b8806c27fc7f84ad65e6
kadir3434 <kadiryesir@gmail.com>
2019-11-22 14:20:29 -0600

    Sum method is added

--- src/test/java/java_code/ExampleForGit.java
+++ src/test/java/java_code/ExampleForGit.java
@@ -8,9 +8,13 @@ public class ExampleForGit implements ClassForInterface {
     public static void main(String[] args) {
         ExampleForGit say = new ExampleForGit();
         say.SayHello();
+        say.Sum(5,6);
     }
 
     public void SayHello() {
         System.out.println("Hello");
+    }
+    public void Sum(int a, int b){
+        System.out.println(a+b);
     }
 }
--- src/test/java/utilities/ClassForInterface.java
+++ src/test/java/utilities/ClassForInterface.java
@@ -2,5 +2,6 @@ package utilities;
 
 public interface ClassForInterface {
     public void SayHello();
+    public void Sum(int a, int b);
 
 }
